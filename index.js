ad22ea3cbed93796a60dfe70cc8229886eff7af5
Will Smithee <wasmithee@gmail.com>
2022-09-09 11:45:36 -0600

    Changed name

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 export default {
-	fetch(request) {
-		if(request.url == "https://my-project.willsmithee.workers.dev") {
-			return new Response('Hello worker!', {
-				headers: {
-					'content-type': 'text/plain',
-				},
-			});
-		}
-		else{
-			return new Response('Error Worker!', {
-				headers: {
-					'content-type': 'text/plain',
-				},
-			});
-		}
-	},
+  fetch(request) {
+    /*
+    // Respond based on if the URL of the request matches what is hardcoded in the foolowing cod block.
+    if (request.url == "https://codeclub-1.willsmithee.workers.dev") {
+      return new Response("Hello worker!", {
+        headers: {
+          "content-type": "text/plain",
+        },
+      });
+    } else {
+      return new Response("Error Worker!", {
+        headers: {
+          "content-type": "text/plain",
+        },
+      });
+    }
+    */
+    // Respond with a sentence containg the request's method, otherwise respond that the request object doesn'r exist
+    if (request) {
+      return new Response(`This request used the ${request.method} method.`);
+    } else {
+      return new Response(`The request object does not exist`);
+    }
+  },
 };
